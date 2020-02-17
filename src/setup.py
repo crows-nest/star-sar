f44b068add52f89dff58a0e2e10b7fe9890fd493
jeff <jeve0658@colorado.edu>
2020-02-17 13:30:03 -0700

    package setup and initialization

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+   name='star SAR',
+   version='0.1',
+   description='a DDF SAR solution',
+   author='Jeff Venicx',
+   author_email='jeve0658@colorado.edu',
+   packages=['crow_rf'], 
+   install_requires=[], #external packages as dependencies
+   python_requires= '>=3.6'
+)
