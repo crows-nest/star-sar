db585dec65fb9ae6f15065850113548865ff52cb
Jeff Venicx <jeve0658@colorado.edu>
2020-03-17 23:03:50 -0600

    commented code fixed plot function

--- src/crow_gis/GIS_topo.py
+++ src/crow_gis/GIS_topo.py
@@ -15,27 +15,49 @@ gdal.UseExceptions()    # Enable exceptions
 class GIS_topo(object):
     """
     abstraction of GDAL raster object
-    prefered as topology only
+    prefered as raster only?
     abstraction of vector types too? 
     """
 
-
     def __init__(self, configfile="config_1.json"):
+        """
+        Initializes GIS object, loads JSON file contains "filename_depth", "cord_of_interest"
+        
+        Parameters
+        ----------
+        configfile : str, optional
+            the JSON file to load configurations from, by default "config_1.json"
+        """
+       
 
         json_file = open(configfile)
         json_data = json.load(json_file)
 
-
         self.filename = json_data["filename_depth"]
         self.cord_of_interest = json_data["cord_of_interest"]
-        
-       
         self.src_ds = self.open_geotiff(self.filename)
+        graph_list = []
+        graph_list.append(self.get_AOE(self.src_ds, self.cord_of_interest))
+        graph_list.append(self.get_AOE(self.src_ds, self.cord_of_interest))
 
-        np_aoe = self.get_AOE(self.src_ds, self.cord_of_interest)
+        self.plot_depth(graph_list)
     
     
     def open_geotiff(self, filename):
+        """
+        loads a geirtif file, file pathing is relative to exection
+        TODO make global file referencing
+        
+        Parameters
+        ----------
+        filename : string
+            name of geotiff file to open
+        
+        Returns
+        -------
+        GDAL raster dataset
+            GDAL raster object
+        """
 
         self.filename = filename
         src_ds = gdal.Open(filename)
@@ -49,6 +71,21 @@ class GIS_topo(object):
         return src_ds
 
     def get_depth_np(self, src_ds, raster_band = 1):
+        """
+        uses a GDAL raster dataset to open first raster band and turn into numpy array
+        
+        Parameters
+        ----------
+        src_ds : GDAL raster dataset
+            GDAL raster object
+        raster_band : int, optional
+            th eband to look up, by default 1
+        
+        Returns
+        -------
+        np.array
+            numpy array containing the depth data
+        """
 
         raster_band = src_ds.GetRasterBand(raster_band)
         np_array = raster_band.ReadAsArray()
@@ -62,6 +99,23 @@ class GIS_topo(object):
     
 
     def get_AOE(self, src_ds, cord_of_interst):
+        """
+        using th GDAL dataset returns the area define by cord_of_interest dict.
+        the cord are defined in terms of lat and long
+        no errors for out if bounds cordinates
+        
+        Parameters
+        ----------
+        src_ds : GDAL raster dataset
+            GDAL raster object
+        cord_of_interst : dict
+            dictionary of coordinates contains: ulx, uly, lrx, lry (Upper Left and Lower Right)
+        
+        Returns
+        -------
+        [type]
+            [description]
+        """
         
         np_depth = self.get_depth_np(src_ds)
 
@@ -82,32 +136,51 @@ class GIS_topo(object):
         return np_aoe        
 
     def get_pixel_coord(self, geo_coord, src_ds):
+        """
+        uses GDAL dataset to pull array index for a cordinate
 
+        
+        Parameters
+        ----------
+        geo_coord : tuple
+            lat and long coordinate
+        src_ds : GDAL raster dataset
+            GDAL raster object
+        
+        Returns
+        -------
+        tuple
+            index of geo cordinates
+        """
         x, y = geo_coord[0], geo_coord[1]
-        forward_transform =  \
-            affine.Affine.from_gdal(*src_ds.GetGeoTransform())
+        forward_transform = affine.Affine.from_gdal(*src_ds.GetGeoTransform())
         reverse_transform = ~forward_transform
         px, py = reverse_transform * (x, y)
         px, py = int(px + 0.5), int(py + 0.5)
-        pixel_coord = px, py
+        pixel_coord = (px, py)
 
         return pixel_coord
 
     def plot_depth(self, list_graph):
-
+        """
+        plots 2D arrays as graphs in matlplotlib
+        accepts lists of 2D arrays to be graphed
+        makes a subplot for each graph
+        
+        Parameters
+        ----------
+        list_graph : list
+            list of 2D graphs to be displayes
+        """
+        
         fig = plt.figure()
+        ax = []
+        num_graphs = len(list_graph)
 
-        ax1 = fig.add_subplot(1, 4, 1)
-        ax1.imshow(orig)
+        for index, graph in enumerate(list_graph):
 
-        ax2 = fig.add_subplot(1, 4, 2)
-        ax2.imshow(aoe_x)
-
-        ax3 = fig.add_subplot(1, 4, 3)
-        ax3.imshow(aoe_y)
-
-        ax4 = fig.add_subplot(1, 4, 4)
-        ax4.imshow(aoe)
+            ax = fig.add_subplot(1, num_graphs, index + 1)
+            ax.imshow(graph)
 
 
         plt.show()
