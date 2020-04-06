b3a15e3ddb9cd671fb49956f0ad28a35aabccf27
jeff <jeve0658@colorado.edu>
2020-04-06 13:48:51 -0600

    requirements update

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
 setuptools.setup(
    name='star SAR',
    version='0.1',
    description='a DDF SAR solution',
    author='Jeff Venicx',
    author_email='jeve0658@colorado.edu',
-   packages=["crows_nest"], 
-   install_requires=[], #external packages as dependencies
+   install_requires=requirements, 
    python_requires= '>=3.6'
 )
